baf43a6f2102f9a692b94a8d609ec43ddcec6a81
Yurikyakyt <dzhatiev80@mail.ru>
2023-01-05 15:12:35 +0300

    format code

--- lab2user.c
+++ lab2user.c
@@ -6,22 +6,22 @@
 #define BUFFER_SIZE 1024
 
 int main(int argc, char *argv[]) {
-  unsigned int pid;
-  FILE *file = fopen("/sys/kernel/debug/lab2/lab2file", "r+");
-  if (file == NULL) {
-    printf("Возникли технические неполадки\n");
-    return 0;
-  }
-  if (sscanf(argv[1], "%x", &pid)) {
-    char *buffer[BUFFER_SIZE];
-    fprintf(file, "pid: %x", pid);
-    while (!feof(file)) {
-      char *result = fgets(buffer, BUFFER_SIZE, file);
-      printf(result);
-    }
-  } else {
-      printf("Что-то пошло не так");	 
-  }
-  fclose(file);
-  return 0;
+	unsigned int pid;
+	FILE *file = fopen("/sys/kernel/debug/lab2/lab2file", "r+");
+	if (file == NULL) {
+		printf("Возникли технические неполадки\n");
+		return 0;
+	}
+	if (sscanf(argv[1], "%x", &pid)) {
+		char *buffer[BUFFER_SIZE];
+		fprintf(file, "pid: %x", pid);
+		while (!feof(file)) {
+			char *result = fgets(buffer, BUFFER_SIZE, file);
+			printf(result);
+		}
+	} else {
+		printf("Что-то пошло не так");	 
+	}
+	fclose(file);
+	return 0;
 }
